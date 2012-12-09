3ff50c435ca7a284383ddc15560f1f4c9c9336a1
toshinori <toshinori@imaizumi.jp>
2012-12-09 18:50:57 +0900

    add setting class

---
+++ app/setting.rb
@@ -0,0 +1,13 @@
+class Setting
+  class << self
+    def evernote_host
+      ENV['evernote_host']
+    end
+    def consumer_key
+      ENV['consumer_key']
+    end
+    def consumer_secret
+      ENV['consumer_secret']
+    end
+  end
+end
