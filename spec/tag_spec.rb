55ff8c9985581ab02e06f023ff6289711716d0aa
toshinori <toshinori@imaizumi.jp>
2013-01-01 14:39:08 +0900

    Tagモデルの基本的なテストを追加。

--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -1,8 +1,14 @@
 describe 'Tag Model' do
 
-  describe '.name' do
-    it 'should not be empty' do
-      Tag.file_name.should.not.empty
+  describe '.file_name' do
+    it 'not empty' do
+      Tag.file_name.should.not.be.empty
+    end
+  end
+
+  describe '.path' do
+    it 'not empty' do
+      Tag.path.should.not.be.empty
     end
   end
 
@@ -15,4 +21,40 @@ describe 'Tag Model' do
     end
   end
 
+  describe '#new' do
+    before do
+      @tag = Tag.new
+    end
+    it 'colums are empty' do
+      @tag.name.should.be.nil
+      @tag.guid.should.be.nil
+      @tag.parentGuid.should.be.nil
+      @tag.updateSequenceNum.should.be.nil
+      @tag.last_used.should.be.nil
+    end
+    it 'not saved' do
+       Tag.all.size.should.equal 0
+     end
+  end
+
+  describe '#create' do
+    before do
+      @tag = Tag.create(name: 'hoge')
+    end
+    it 'saved' do
+      Tag.all.size.should.not.equal 0
+    end
+  end
+
+  describe '.save and load' do
+    before do
+      Tag.truncate
+      Tag.create(name: 'hoge')
+      Tag.save
+      @tags = Tag.all
+    end
+    it 'not empty' do
+      @tags.should.not.be.empty
+    end
+  end
 end
