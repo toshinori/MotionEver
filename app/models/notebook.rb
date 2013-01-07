3b2f1cf68723a18e1978747aa364d57cdb9c2f95
toshinori <toshinori@imaizumi.jp>
2013-01-07 23:24:52 +0900

    last_usedの初期値に今日の日付を設定するようにした。 日付と使用回数でソートする処理を追加。

--- app/models/notebook.rb
+++ app/models/notebook.rb
@@ -8,13 +8,17 @@ class Notebook < ModelBase
     last_used: :date
 
   def initialize(options = {})
-    # updateSequenceNum、defaultNotebookに初期値を設定する
+    # updateSequenceNum、defaultNotebook、last_usedに初期値を設定する
     unless options.has_key?(:updateSequenceNum)
       options[:updateSequenceNum] = 0
     end
 
     unless options.has_key?(:defaultNotebook)
       options[:defaultNotebook] = false
+    end
+
+    unless options.has_key?(:last_used)
+      options[:last_used] = Time.now
     end
 
     super(options)
@@ -24,6 +28,15 @@ class Notebook < ModelBase
     def find_by_name name
       Notebook.where(:name).eq(name).first
     end
+
+    def order_by_date_and_num
+      Notebook.order do |x, y|
+        y.updateSequenceNum <=> x.updateSequenceNum
+      end.order do |x, y|
+        y.last_used <=> x.last_used
+      end.all
+    end
+
   end
 
 end
--- spec/notebook_spec.rb
+++ spec/notebook_spec.rb
@@ -10,6 +10,11 @@ describe Notebook do
     it 'defaultNotebook should be false' do
       @notebook.defaultNotebook.should.equal false
     end
+    it 'last_used should be today' do
+      @notebook.last_used.year.should.equal Time.now.year
+      @notebook.last_used.month.should.equal Time.now.month
+      @notebook.last_used.day.should.equal Time.now.day
+    end
   end
 
   describe '.find_by_name' do
@@ -22,6 +27,27 @@ describe Notebook do
     it 'should return Notebook(name is hoge)' do
       @notebook.should.not.nil
       @notebook.name.should.equal 'hoge'
+    end
+  end
+
+  describe '.order_by_date_and_num' do
+    before do
+      now = Time.now
+      Notebook.truncate
+      Notebook.create name: 'hoge',
+        updateSequenceNum: 100
+      Notebook.create name: 'fuga',
+        updateSequenceNum: 10,
+        last_used: now + 10000
+      Notebook.create name: 'hige',
+        updateSequenceNum: 20,
+        last_used: now + 10000
+      @notebooks = Notebook.order_by_date_and_num
+    end
+    it 'should order by last_used, updateSequenceNum desc' do
+      @notebooks[0].name.should.equal 'hige'
+      @notebooks[1].name.should.equal 'fuga'
+      @notebooks[2].name.should.equal 'hoge'
     end
   end
 end
